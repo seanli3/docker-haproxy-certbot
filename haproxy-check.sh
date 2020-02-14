a135548a6f63315aa20d5bb51786bd43b9f52c94
Paul Desplanque <60922906+lonequantum@users.noreply.github.com>
2020-02-14 12:08:55 +0100

    Fix/Simplify the way to check configs

--- haproxy-check.sh
+++ haproxy-check.sh
@@ -2,10 +2,12 @@
 
 cfg_file=$1
 
+cd /config
+
 if [ -z "$1" ]; then
-    cfg_file="/config/haproxy.cfg"
+    cfg_file="haproxy.cfg"
     echo "No argument supplied, using" ${cfg_file}
-    echo "To use config from host try \"(docker exec -i container haproxy-check -) < path_to_your_config\""
+    echo "To check another config from host, put your test.cfg in the same directory as haproxy.cfg and use \"docker exec container_name haproxy-check test.cfg\""
 fi
 
 /usr/local/sbin/haproxy -c -V -f "${cfg_file}"
