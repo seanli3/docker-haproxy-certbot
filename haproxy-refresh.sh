328c86916dec5599d1d059031ce39ae20ae70c32
Paul Desplanque <60922906+lonequantum@users.noreply.github.com>
2020-02-14 09:41:53 +0100

    Delete /etc/letsencrypt/live/README on refresh

--- haproxy-refresh.sh
+++ haproxy-refresh.sh
@@ -4,11 +4,13 @@ HA_PROXY_DIR=/usr/local/etc/haproxy
 LE_DIR=/etc/letsencrypt/live
 DOMAINS=$(ls ${LE_DIR})
 
-# update certs for HA Proxy
+rm -f ${LE_DIR}/README
+
+# update certs for HAProxy
 for DOMAIN in ${DOMAINS}
 do
  cat ${LE_DIR}/${DOMAIN}/fullchain.pem ${LE_DIR}/${DOMAIN}/privkey.pem > ${HA_PROXY_DIR}/certs.d/${DOMAIN}.pem
 done
 
-# restart haproxy
+# restart HAProxy
 exec /usr/bin/haproxy-restart
