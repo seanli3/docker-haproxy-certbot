801fe97e45f9a98e65735032d844911fedb5c64d
Paul Desplanque <60922906+lonequantum@users.noreply.github.com>
2020-02-14 09:59:42 +0100

    Update haproxy-refresh.sh

--- haproxy-refresh.sh
+++ haproxy-refresh.sh
@@ -7,9 +7,8 @@ DOMAINS=$(ls ${LE_DIR})
 rm -f ${LE_DIR}/README
 
 # update certs for HAProxy
-for DOMAIN in ${DOMAINS}
-do
- cat ${LE_DIR}/${DOMAIN}/fullchain.pem ${LE_DIR}/${DOMAIN}/privkey.pem > ${HA_PROXY_DIR}/certs.d/${DOMAIN}.pem
+for DOMAIN in ${DOMAINS}; do
+    cat ${LE_DIR}/${DOMAIN}/fullchain.pem ${LE_DIR}/${DOMAIN}/privkey.pem > ${HA_PROXY_DIR}/certs.d/${DOMAIN}.pem
 done
 
 # restart HAProxy
